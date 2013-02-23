b12767f9d5f511c4eda34f74ce692b92a7a247be
lhanalytics <damian@lakehillanalytics.com>
2013-02-23 09:20:53 -0500

    parser updates

--- KEXPParser.py
+++ KEXPParser.py
@@ -5,8 +5,10 @@ import os
 import urllib2
 from bs4 import BeautifulSoup
 import bleach
+import logging
 import lha_utils
 import MySQLdb
+import boto
 
 
 """
@@ -15,105 +17,286 @@ A whole bunch of string identifiers to make life a little easier
 # This is the basic address of the location we've saved the IR HTML
 # Adding on the page parameter will get us from page to page.
 BASE_LIST_URL = 'file:///Users/damian/Documents/Dropbox/Lake-Hill-Analytics/Code/Projects/KEXP/kexp-source-playlists/'
-KEY_NAME = 'kexp-source-playlists-2013-02-01-9am.html'
 DIR_BASE = '/Users/damian/Documents/Dropbox/Lake-Hill-Analytics/Code/Projects/KEXP/kexp-source-playlists/'
 
-# set the main URL to grab
-URL = BASE_LIST_URL + KEY_NAME
+LHA_AWS_ACCESS_KEY = 'AKIAJWV4X2TDHPBE5WIQ'
+LHA_AWS_SECRET_KEY = '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'
 
 # set the bucket name for S3
-BUCKET_NAME = 'lha_kexp_playlist_bucket'
-
-# set the directory name
-DIR_NAME = DIR_BASE + KEY_NAME
-
-# Name an outfile
-OUT = 'kexp-playlist-output.txt'
-
+BUCKET_NAME = 'lha_kexp_playlist_small_set'
 """
 Done setting up strings
 """
 
+"""
+SET UP LOG
+"""
+os.chdir(DIR_BASE)
+
+logging.basicConfig(
+	filename='aLOG-KEXP_Parser.txt',
+	level=logging.INFO,
+	filemode="w",
+	format='%(levelname)s %(asctime)s %(message)s',
+	datefmt='%m/%d/%Y %I:%M:%S %p')
+
+logging.info("Started Page Grabs")
+
+"""
+LOG SETUP COMPLETE
+"""
+
 # Connect to a database
-db = MySQLdb.connect(host="localhost", port=3306, user="root", passwd="hobart93", db="lha_kexp_playlist")
-cursor = db.cursor()
+try:
+	db = MySQLdb.connect(host="localhost", port=3306, user="root", passwd="hobart93", db="lha_kexp_playlist")
+	cursor = db.cursor()
+	logging.info("Successfully connected to database")
+except:
+	logging.info("Failed to connect to database")
 
 # Get the file from S3
-lha_utils.get_s3_data(BUCKET_NAME, KEY_NAME, DIR_NAME)
 
-try:
-	page = urllib2.urlopen(URL)
-	print 'Grabbed: ' + URL
+s3 = boto.connect_s3(aws_access_key_id=LHA_AWS_ACCESS_KEY, aws_secret_access_key=LHA_AWS_SECRET_KEY)
+bucket = s3.get_bucket(BUCKET_NAME)
 
-except urllib2.URLError:
-	print 'Failed to fetch: ' + URL
+bucket_list = bucket.list()
+for l in bucket_list:
+	key_string = str(l.key)
 
-try:
-	soup = BeautifulSoup(page)
-	print 'Made Soup'
+	# set the URL name for BeautifulSoup
+	URL = BASE_LIST_URL + key_string
 
-except HTMLParser.HTMLParseError:
-	print 'Failed to parse: ' + URL
+	# set the directory name for local file
+	DIR_NAME = DIR_BASE + key_string
 
-"""
-Grab the items we care about
-There are 10 categories
-"""
-# Start with single items
-showHost = soup.findAll("div", attrs={'class': 'ShowHost'})
-airDate = soup.findAll("div", attrs={'class': 'ShowAirDate'})
-showName = soup.findAll("div", attrs={'class': 'ShowName'})
+	try:
+#		lha_utils.get_s3_data(BUCKET_NAME, KEY_NAME, DIR_NAME)
+		l.get_contents_to_filename(DIR_NAME)
+		logging.info("Successfully grabbed file: " + key_string)
+	except:
+		logging.info("Failed to grab: " + key_string)
 
-# Strip tags as needed
-showHost[0].contents[0] = bleach.clean(showHost[0].contents[0], tags=[], strip=True)
-showName[0].contents[1] = bleach.clean(showName[0].contents[1], tags=[], strip=True)
+	try:
+		page = urllib2.urlopen(URL)
+		logging.info("Grabbed: " + URL)
 
-# Now get all the ones that are in the playlist
-artistNames = soup.findAll("div", attrs={'class': 'ArtistName'})
-releaseNames = soup.findAll("div", attrs={'class': 'ReleaseName'})
-trackNames = soup.findAll("div", attrs={'class': 'TrackName'})
-airTime = soup.findAll("div", attrs={'class': 'AirDate'})
-DJComments = soup.findAll("div", attrs={'class': 'CommentText'})
+	except urllib2.URLError:
+		logging.info("Failed to fetch: " + URL)
 
-f = open(OUT, "w")
+	try:
+		soup = BeautifulSoup(page)
+		logging.info("Made Soup")
 
-f.write('Host: ' + showHost[0].contents[0] + '\n')
-f.write('Air Date: ' + airDate[0].contents[0] + '\n')
-f.write('Show Name: ' + showName[0].contents[1] + '\n\n')
+	except:
+		logging.info("Failed to parse: " + URL)
 
-# Check if this host exists in the db already
-try:
-	cursor.execute("""SELECT name FROM hosts WHERE name = (%s)""", showHost[0].contents[0])
-	data = cursor.fetchall()
-	rows = cursor.rowcount
-	print rows
-	if rows > 0:
-		# there is only one of this person so we should be safe to go this route.
-		print 'Record already exists: ' + str(data[0][0])
-	else:
-		print 'Creating Data'
+	"""
+	Grab the items we care about
+	There are 10 categories
+	"""
+	# Start with single items
+	showHost = soup.findAll("div", attrs={'class': 'ShowHost'})
+	airDate = soup.findAll("div", attrs={'class': 'ShowAirDate'})
+	showName = soup.findAll("div", attrs={'class': 'ShowName'})
+
+	# Strip tags as needed
+	showHost[0].contents[0] = bleach.clean(showHost[0].contents[0], tags=[], strip=True)
+	showName[0].contents[1] = bleach.clean(showName[0].contents[1], tags=[], strip=True)
+	strippedDate = airDate[0].contents[0].split(' - ')[0]
+
+	# Now get all the ones that are in the playlist
+	artistNames = soup.findAll("div", attrs={'class': 'ArtistName'})
+	releaseNames = soup.findAll("div", attrs={'class': 'ReleaseName'})
+	trackNames = soup.findAll("div", attrs={'class': 'TrackName'})
+	airTime = soup.findAll("div", attrs={'class': 'AirDate'})
+#	DJComments = soup.findAll("div", attrs={'class': 'CommentText'})
+
+	"""
+	PARSING FINISHED, NOW WRITE TO MYSQL
+	"""
+
+	"""
+	HOSTS
+	"""
+	try:
+		cursor.execute("""SELECT name FROM hosts WHERE name = (%s)""", showHost[0].contents[0])
+		data = cursor.fetchall()
+		rows = cursor.rowcount
+		if rows > 0:
+			# there is only one of this person so we should be safe to go this route.
+			logging.info("Record already exists: " + str(data[0][0]))
+		else:
+			logging.info("Creating Data")
+			try:
+				cursor.execute("""INSERT INTO hosts (name) VALUES (%s)""", showHost[0].contents[0])
+				db.commit()
+				logging.info("Successfully added: " + showHost[0].contents[0])
+			except:
+				db.rollback()
+				logging.info("Failed to insert data")
+	except:
+		logging.info("Data Check failed")
+
+	"""
+	END HOSTS
+	"""
+
+	"""
+	SHOW NAME
+	"""
+	try:
+		cursor.execute("""SELECT name FROM shows WHERE name = (%s)""", showName[0].contents[1])
+		data = cursor.fetchall()
+		rows = cursor.rowcount
+		if rows > 0:
+			# there is only one of this person so we should be safe to go this route.
+			logging.info("Record already exists: " + str(data[0][0]))
+		else:
+			logging.info("Creating Data")
+			try:
+				cursor.execute("""INSERT INTO shows (name) VALUES (%s)""", showName[0].contents[1])
+				db.commit()
+				logging.info("Successfully added: " + showName[0].contents[1])
+			except:
+				db.rollback()
+				logging.info("Failed to insert data")
+	except:
+		logging.info("Data Check failed")
+
+	"""
+	END SHOW NAME
+	"""
+
+	for i in range(len(artistNames)):
+		artistNames[i].contents[0] = bleach.clean(artistNames[i].contents[0], tags=[], strip=True)
+		airTime[i].contents[0] = bleach.clean(airTime[i].contents[0], tags=[], strip=True)
+#		DJComments[i].contents[2] = DJComments[i].contents[2].strip('\t\n')
+
+		"""
+		ARTIST
+		"""
 		try:
-			cursor.execute("""INSERT INTO hosts (name) VALUES (%s)""", showHost[0].contents[0])
+			cursor.execute("""SELECT name FROM artist WHERE name = (%s)""", artistNames[i].contents[0])
+			data = cursor.fetchall()
+			rows = cursor.rowcount
+			if rows > 0:
+				# there is only one of this person so we should be safe to go this route.
+				logging.info("Record already exists: " + str(data[0][0]))
+			else:
+				logging.info("Creating Data")
+				try:
+					cursor.execute("""INSERT INTO artist (name) VALUES (%s)""", artistNames[i].contents[0])
+					db.commit()
+					logging.info("Successfully added: " + artistNames[i].contents[0])
+				except:
+					db.rollback()
+					logging.info("Failed to insert data")
+		except:
+			logging.info("Data Check failed (artist name)")
+
+		"""
+		END ARTISTS
+		"""
+
+		"""
+		ALBUM
+		"""
+		try:
+			cursor.execute("""SELECT name FROM albums WHERE name = (%s)""", releaseNames[i].contents[0])
+			data = cursor.fetchall()
+			rows = cursor.rowcount
+			if rows > 0:
+				# there is only one of this person so we should be safe to go this route.
+				logging.info("Record already exists: " + str(data[0][0]))
+			else:
+				logging.info("Creating Data")
+				try:
+					cursor.execute("""INSERT INTO albums (name) VALUES (%s)""", releaseNames[i].contents[0])
+					db.commit()
+					logging.info("Successfully added: " + releaseNames[i].contents[0])
+				except:
+					db.rollback()
+					logging.info("Failed to insert data")
+		except:
+			logging.info("Data Check failed (album name)")
+
+		"""
+		END ALBUM
+		"""
+
+		"""
+		SONG
+		"""
+		try:
+			cursor.execute("""SELECT title FROM songs WHERE title = (%s)""", trackNames[i].contents[0])
+			data = cursor.fetchall()
+			rows = cursor.rowcount
+			if rows > 0:
+				# there is only one of this person so we should be safe to go this route.
+				logging.info("Record already exists: " + str(data[0][0]))
+			else:
+				logging.info("Creating Data")
+				try:
+					cursor.execute("""SELECT id FROM artist WHERE name = (%s)""", artistNames[i].contents[0])
+					data = cursor.fetchall()
+					id_artist = data[0][0]
+
+					cursor.execute("""SELECT id FROM albums WHERE name = (%s)""", releaseNames[i].contents[0])
+					data = cursor.fetchall()
+					id_album = data[0][0]
+
+					cursor.execute("""INSERT INTO songs (title, artistID, albumID) VALUES (%s, %s, %s)""",
+						(trackNames[i].contents[0], id_artist, id_album))
+					db.commit()
+					logging.info("Successfully added: " + trackNames[i].contents[0])
+				except:
+					db.rollback()
+					logging.info("Failed to insert data")
+		except:
+			logging.info("Data Check failed (song name)")
+
+		"""
+		END SONG
+		"""
+
+		"""
+		PLAYLIST
+		"""
+		try:
+			logging.info("Starting playlist Insert: " + trackNames[i].contents[0])
+			cursor.execute("""SELECT id FROM songs WHERE title = (%s)""", trackNames[i].contents[0])
+			data = cursor.fetchall()
+			id_song = data[0][0]
+
+			cursor.execute("""SELECT id FROM artist WHERE name = (%s)""", artistNames[i].contents[0])
+			data = cursor.fetchall()
+			id_artist = data[0][0]
+
+			cursor.execute("""SELECT id FROM albums WHERE name = (%s)""", releaseNames[i].contents[0])
+			data = cursor.fetchall()
+			id_album = data[0][0]
+
+			cursor.execute("""SELECT id FROM shows WHERE name = (%s)""", showName[0].contents[1])
+			data = cursor.fetchall()
+			id_show = data[0][0]
+
+			cursor.execute("""SELECT id FROM hosts WHERE name = (%s)""", showHost[0].contents[0])
+			data = cursor.fetchall()
+			id_host = data[0][0]
+
+			cursor.execute("""INSERT INTO playlist (songID, artistID, albumID, hostID, showID, date, time)
+				VALUES (%s, %s, %s, %s, %s, %s, %s)""",
+				(id_song, id_artist, id_album, id_host, id_show, strippedDate, airTime[i].contents[0]))
 			db.commit()
-			print 'Successfully added: ' + showHost[0].contents[0]
+			logging.info("Successfully added (playlist): " + trackNames[i].contents[0])
 		except:
 			db.rollback()
-			print 'Failed to insert data'
-except:
-	print 'Failed to check properly'
+			logging.info("Failed to insert data")
 
-for i in range(len(artistNames)):
-	artistNames[i].contents[0] = bleach.clean(artistNames[i].contents[0], tags=[], strip=True)
-	airTime[i].contents[0] = bleach.clean(airTime[i].contents[0], tags=[], strip=True)
-	DJComments[i].contents[2] = DJComments[i].contents[2].strip('\t\n')
+		"""
+		END PLAYLIST
+		"""
 
-	f.write('Artist: ' + artistNames[i].contents[0] + '\n')
-	f.write('Album: ' + releaseNames[i].contents[0] + '\n')
-	f.write('Song: ' + trackNames[i].contents[0] + '\n')
-	f.write('Air Time: ' + airTime[i].contents[0] + '\n')
-	f.write('DJ Comment: ' + DJComments[i].contents[2] + '\n')
-
-f.close()
-os.remove(DIR_NAME)
+	os.remove(DIR_NAME)
 cursor.close()
 db.close()
