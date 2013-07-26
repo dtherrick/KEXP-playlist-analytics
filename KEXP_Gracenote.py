40fe7aef2b6709f189d7a956a27a01f526c22e1f
lhanalytics <damian@lakehillanalytics.com>
2013-07-25 21:46:51 -0400

    latest commit

--- KEXP_Gracenote.py
+++ KEXP_Gracenote.py
@@ -14,17 +14,21 @@ A whole bunch of string identifiers to make life a little easier
 BASE_LIST_URL = 'file:///home/ec2-user/kexp-project/'
 DIR_BASE = '/home/ec2-user/kexp-project/'
 
+"""
+Access into AWS 
+"""
 LHA_AWS_ACCESS_KEY = 'AKIAJWV4X2TDHPBE5WIQ'
 LHA_AWS_SECRET_KEY = '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'
 
-# set the bucket name for S3
-BUCKET_NAME = 'lha_kexp_playlist_bucket'
 """
-Done setting up strings
+GRACE NOTE VARIABLES
 """
 
+clientID = '2408704-C81580978847D7212FF39B94C4F65088'
+userID = pygn.register(clientID)
+
 """
-SET UP LOG
+SET UP LOGGGING
 """
 logging.basicConfig(
 	filename='aLOG-KEXP_Gracenote.txt',
@@ -33,13 +37,13 @@ logging.basicConfig(
 	format='%(levelname)s %(asctime)s %(message)s',
 	datefmt='%m/%d/%Y %I:%M:%S %p')
 
-logging.info("Started Page Grabs")
+logging.info("Starting Gracenote API grabbing")
 
 """
 LOG SETUP COMPLETE
 """
 
-# Connect to a database
+# Connect to the KEXP source database
 try:
 	db = MySQLdb.connect(host="lhakexpmaster.c1kvj0gfryqy.us-east-1.rds.amazonaws.com", port=3306, user="lha_kexp_proof", passwd="Statesman1997", db="lhaKEXPMaster")
 	cursor = db.cursor()
@@ -47,22 +51,31 @@ try:
 except:
 	logging.info("Failed to connect to database")
 
+
 """
-VARIABLES
+QUERY THE DATABASE FOR THE SET OF ALL SONGS
 """
 
-clientID = '2408704-C81580978847D7212FF39B94C4F65088'
-userID = pygn.register(clientID)
 
+"""
+LOOP THROUGH ALL ENTRIES IN THE QUERY SET TO GET SONG INFORMATION
+"""
 metadata = pygn.searchTrack(clientID, userID, 'Father John Misty', 'Fear Fun', 'Hollywood Forever Cemetary Sings')
+
+
+"""
+NOW ADD TO THE DATABASE
+"""
 
 print metadata['track_title']
 
 for i in metadata['mood']:
 	print metadata['mood'][i]['TEXT']
-	
+
 for i in metadata['genre']:
 	print metadata['genre'][i]['TEXT']
+
+print metadata
 
 cursor.close()
 db.close()
